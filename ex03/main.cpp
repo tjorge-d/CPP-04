6457f8da8147d8f59e9afbd9960e150751c202ce
Tiago Jorge Da Silva Oliveira <tjorge-d@c1r7s13.42lisboa.com>
2024-10-16 12:00:40 +0100

    Finished the project

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -27,8 +27,9 @@ void	test1()
 
 void	test2()
 {
-	std::cout << "\n==========| TEST 2 |==========\n\n";
+	std::cout << "\n==========| TEST 2 |==========\n";
 
+	std::cout << "\nMateriaSource :\n\n";
 	MateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	MateriaSource* copy = new MateriaSource(*src);
@@ -37,23 +38,79 @@ void	test2()
 	copy->learnMateria(new Cure());
 
 	std::cout << "\nsrc ("<< src <<") :\n";
-	AMateria *t1 = src->createMateria("cure");
+	AMateria *m1 = src->createMateria("cure");
 
 	std::cout << "\ncopy ("<< copy <<") :\n";
-	AMateria *t2 = copy->createMateria("ice");
+	AMateria *m2 = copy->createMateria("ice");
 
 	delete src;
 	std::cout << "\nDeleted src and still can use copy:\n";
-	AMateria *t3 = copy->createMateria("ice");
+	AMateria *m3 = copy->createMateria("ice");
 
+	std::cout << "\nEvery materia has diferent addresses:\n";
+	std::cout << "m1 (" << m1 << ")\n";
+	std::cout << "m2 (" << m2 << ")\n";
+	std::cout << "m3 (" << m3 << ")\n";
 	delete copy;
-	delete t1;
-	delete t2;
-	delete t3;
+
+	std::cout << "\nCharacters (using AMaterias from above) :\n\n";
+	Character* alvares = new Character("Alvares");
+	Character* cabral = new Character("Cabral");
+	Cure* cure = new Cure();
+
+	alvares->equip(m2);
+	alvares->equip(cure);
+	cabral->equip(m3);
+
+	*cabral = *alvares;
+
+	cabral->use(0, *alvares);
+	cabral->use(1, *alvares);
+
+	std::cout << "\nCabral now has Alvares name and AMaterias but they use different addresses:\n";
+	std::cout << "Alvares (" << alvares << ")\n";
+	std::cout << "Cabral (" << cabral << ")\n";
+
+	delete alvares;
+	delete cabral;
+
+	std::cout << "This test wont have leaks even tho we didn't delete m3\n" <<\
+		"This is because when using the = operator on cabral everything he had was deleted\n";
+}
+
+void	test3()
+{
+	std::cout << "\n==========| TEST 3 |==========\n";
+	Character* diogo = new Character("Diogo");
+	Character* cao = new Character("Cao");
+	Ice *ice1 = new Ice();
+	Ice *ice2 = new Ice();
+	Cure *cure1 = new Cure();
+	Cure *cure2 = new Cure();
+	Cure *cure3 = new Cure();
+
+	diogo->equip(ice1);
+	diogo->equip(ice2);
+	diogo->equip(cure1);
+	diogo->equip(cure2);
+	diogo->equip(cure3);
+	diogo->unequip(1);
+	diogo->unequip(3);
+
+	cao->use(0, *diogo);
+	cao->equip(ice2);
+	cao->equip(cure2);
+	cao->use(0, *diogo);
+	(void)cao;
+
+	delete diogo;
+	delete cao;
+	delete cure3;
 }
 
 int main()
 {
 	test1();
 	test2();
+	test3();
 }
